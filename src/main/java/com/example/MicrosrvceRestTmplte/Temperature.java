9364aad32d80b05246b6108f987060d4fe2d72ea
prudhiviraj <prudhviraj.gundabathula@gmail.com>
2018-04-22 21:21:57 -0500

    first commit of micro1 for communicating b/w 2microsrvcs

---
+++ src/main/java/com/example/MicrosrvceRestTmplte/Temperature.java
@@ -0,0 +1,19 @@
+package com.example.MicrosrvceRestTmplte;
+
+public class Temperature {
+	private int celsius;
+	private String country;
+	
+	public int getCelsius() {
+		return celsius;
+	}
+	public void setCelsius(int celsius) {
+		this.celsius=celsius;
+	}
+	public String getCountry() {
+		return country;
+	}
+    public void setCountry(String country) {
+    	this.country=country;
+    }
+}
