15d530396576b577a51a7e5a13f2655b2c1cc023
prudhiviraj <prudhviraj.gundabathula@gmail.com>
2018-04-22 23:59:19 -0500

    docker spring boot

---
+++ docker-spring-boot-prog/src/main/java/com/egiants/LLC/dockerspringbootprog/DockerSpringBootProgApplication.java
@@ -0,0 +1,12 @@
+package com.egiants.LLC.dockerspringbootprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootProgApplication.class, args);
+	}
+}
---
+++ docker-spring-boot-prog/src/test/java/com/egiants/LLC/dockerspringbootprog/DockerSpringBootProgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.egiants.LLC.dockerspringbootprog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerSpringBootProgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
